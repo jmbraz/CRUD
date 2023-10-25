28a940e05d95d42dc5f9880f8464d0658c04ba9f
João Marcos Da Silva Braz <TIJ0155511@aluno.firjansenaisesi.com.br>
2023-10-25 11:46:28 -0300

    Atualizando projeto HELLO.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 
 const express = require('express')
+//Passa a informação do express para a constante express.
 const app = express()
+//Grupo de funções do express.
 const port = 3000
+//Define a porta.
 app.use(express.json());
+//Responsável pela tradução de requisições HTTP para JSON.
 
 let database = [
 
@@ -17,83 +21,72 @@ let database = [
         name: "zarbmj"
     }
 ]
-
+//Banco de dados, contém lista de informações.
 
 app.get('/users', (request, response) => {
   response.json(database);
 })
 
+//Obter usúarios do banco de dados.
 
 app.get('/users/:id',  (request, response) => { 
-//Buscar ID da requisição.
   const idUser = request.params.id;
-
-
-//Buscar o usuário no database.
+//Buscar ID da requisição.
 
 const user = database.filter((usuario) => usuario.id === idUser);
-
 response.json(user);
-
+//Buscar o usuário no database.
 })
 
 app.post("/users", (request, response) => {
-
-//Pegar o corpo da requisição.
-
+//Responsável por enviar informações.
 const body = request.body;
-
+//Pegar o corpo da requisição.
 const newUser = {
 
   id: (database.length+1).toString(),
   name: body.name
 }
-
-database.push(newUser);
-
-response.json(database);
-
 //Criar um novo objeto a partir desse corpo.
-
+database.push(newUser);
 //Adicionar novo objeto no banco.
-
+response.json(database);
 //Responder a requisição com o banco completo.
 
 })
 
 app.delete("/users/:id", (request, response) => {
-
-  //Pegar o id da requisição.
+  //Responsável por deletar informações. 
   const idUser = request.params.id;
-  //Percorrer o banco e buscar por id da requisição.
-
-
-
+  //Pegar o id da requisição.
   database = database.filter((usuario) => usuario.id != idUser);
-
+  //Percorrer o banco e buscar por ID da requisição.
   response.json(database);
+  //Responder com o banco atualizado.
   })
 
   app.patch("/users/:id", (request, response)=>{
-
+    //Responsável por atualizar informações.
     const idUser = request.params.id;
-
+    //Pegar o id da requisição.
     const body = request.body;
-
+    //Pegar o corpo da requisição.
     database = database.map((usuario) => {
-
       if(usuario.id === idUser){
-
+    //Atualizar as informações.
         usuario.name = body.name;
       }
       return usuario
-
   })
+    //Percorrer o banco.
+
+
 
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+//Descobre a porta do app.
 
 
